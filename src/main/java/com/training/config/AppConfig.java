0e9341a795824f876263482c852827f29b7c6dea
aixinjian <229380496@qq.com>
2018-09-30 14:44:58 +0800

    恢复配置

--- src/main/java/com/training/config/AppConfig.java
+++ src/main/java/com/training/config/AppConfig.java
@@ -1,11 +1,18 @@
 package com.training.config;
 
+import com.training.commodity.service.impl.CommodityServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan("com.training")
+@ComponentScan("com.training.*")
 @EnableAspectJAutoProxy
 public class AppConfig {
+    @Bean
+    public CommodityServiceImpl commodityService(){
+        CommodityServiceImpl commodityService = new CommodityServiceImpl();
+        return commodityService;
+    }
 }
